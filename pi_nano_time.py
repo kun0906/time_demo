c272133af83a17aea29c6a4d16020a217a700646
kunyang <kun.bj@foxmail.com>
2021-03-25 15:46:12 -0400

    add pi_nano_time.py

--- pi_nano_time.py
+++ pi_nano_time.py
@@ -12,6 +12,9 @@ import numpy as np
 
 import pstats
 
+print(np.show_config())
+
+
 def foo(nums=50):
     s = 0
     is_numpy = True
@@ -26,6 +29,30 @@ def foo(nums=50):
         s = 0
         for i in range(10000000):
             s += i
+
+
+def time_timing(name=''):
+    start = time.time()
+
+    foo()
+    # time.sleep(1)
+
+    end = time.time()
+
+    return end - start
+
+
+def ctime_timing(name=''):
+    pr = cProfile.Profile(time.time)
+    pr.enable()
+
+    foo()
+    # time.sleep(1)
+
+    pr.disable()
+    ps = pstats.Stats(pr).sort_stats('line')  # cumulative
+
+    return ps.total_tt
 
 
 def process_timing(name=''):
@@ -54,12 +81,24 @@ def perf_counter_timing(name=''):
     return ps.total_tt
 
 
-n_repeats = 10
+n_repeats = 20
+
+res = []
+for i in range(n_repeats):
+    res.append(time_timing())
+print(f'time_time: {np.mean(res):.5f}+/-{np.std(res):.5f}')
+
+res = []
+for i in range(n_repeats):
+    res.append(ctime_timing())
+print(f'cprofile time: {np.mean(res):.5f}+/-{np.std(res):.5f}')
+
 res = []
 for i in range(n_repeats):
     res.append(process_timing())
-print(f'process_time: {np.mean(res):.5f}+/-{np.std(res):.5f}')
+print(f'cprofile process_time: {np.mean(res):.5f}+/-{np.std(res):.5f}')
 
+res = []
 for i in range(n_repeats):
     res.append(perf_counter_timing())
-print(f'perf_counter: {np.mean(res):.5f}+/-{np.std(res):.5f}')
+print(f'cprofile perf_counter: {np.mean(res):.5f}+/-{np.std(res):.5f}')
